60192b9a1ddef90ac4240272507f5fecc1ccdbcd
Naiba <1@5.nu>
2018-08-13 19:55:32 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 rm -rf dist &&\
     npm run build && cp submit.php dist &&\
     cd dist &&\
+    ssh -t -p1987 root@web.sx.p4x.com 'rm -rf /home/www/mentuo/* && ls -al /home/www/mentuo/' &&\
     scp -P 1987 * root@web.sx.p4x.com:/home/www/mentuo
