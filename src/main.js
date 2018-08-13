f016b96ac924cbbfaadcec2b152997e500c6165f
Naiba <1@5.nu>
2018-08-13 20:35:50 +0800

    谷歌统计

--- src/main.js
+++ src/main.js
@@ -3,16 +3,22 @@
 import Vue from 'vue/dist/vue.esm.js'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
 // 引入element组件库
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'element-ui/lib/theme-chalk/display.css';
-
-Vue.use(ElementUI);
-
 import '../static/styles/home.less';
 
+Vue.use(ElementUI);
+Vue.use(VueAnalytics, {
+    id: 'UA-111315498-11',
+    router,
+    autoTracking: {
+        pageviewOnLoad: false
+    }
+})
 
 new Vue({
     el: '#app',
