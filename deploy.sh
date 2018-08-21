22d35691100f9b4910e608b9b47dd923269d3496
Naiba <1@5.nu>
2018-08-22 00:02:17 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ rm -rf dist &&\
     npm run build && cp submit.php dist &&\
     cd dist &&\
     ssh -t -p1987 root@web.sx.p4x.com 'rm -rf /home/www/mentuo/* && ls -al /home/www/mentuo/' &&\
-    scp -P 1987 * root@web.sx.p4x.com:/home/www/mentuo
+    rsync -rvz -e 'ssh -p 1987' --progress --remove-sent-files . root@web.sx.p4x.com:/home/www/mentuo
