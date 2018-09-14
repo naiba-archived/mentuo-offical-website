aaed44a74e2148a14972b619e541a24fb7c38751
Naiba <1@5.nu>
2018-09-14 11:28:21 +0800

    upload deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 rm -rf dist &&\
-    npm run build && cp submit.php static dist &&\
+    npm run build && cp -rv submit.php static dist &&\
     cd dist &&\
     ssh -t -p1987 root@web.sx.p4x.com 'rm -rf /home/www/mentuo/* && ls -al /home/www/mentuo/' &&\
     rsync -rvz -e 'ssh -p 1987' --progress --remove-sent-files . root@web.sx.p4x.com:/home/www/mentuo
