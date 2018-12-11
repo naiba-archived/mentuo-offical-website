524e1b949d3765c2040693ce31fbab306434a9a0
Naiba <1@5.nu>
2018-12-11 18:34:41 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 rm -rf dist &&\
     npm run build && cp -rv submit.php static dist &&\
     cd dist &&\
-    ssh -t -p1987 root@web.sx.p4x.com 'rm -rf /home/www/mentuo/* && ls -al /home/www/mentuo/' &&\
-    rsync -rvz -e 'ssh -p 1987' --progress --remove-sent-files . root@web.sx.p4x.com:/home/www/mentuo
+    ssh -t -p60030 root@ca.sx.p4x.com 'rm -rf /NAIBA/www/mentuo/* && ls -al /NAIBA/www/mentuo/' &&\
+    rsync -rvz -e 'ssh -p 60030' --progress --remove-sent-files . root@ca.sx.p4x.com:/NAIBA/www/mentuo/
